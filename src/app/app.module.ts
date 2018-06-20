e6b93efcaec013c80101e1633d4ec7e4bd513e01
rvaldez <rvaldez@apptio.com>
2018-06-20 11:06:24 -0700

    minor additions to testComponent and ngOnInit lifecycle method added on PersonComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserComponent } from './feature/user/user.component';
 import { PersonComponent } from './feature/person/person.component';
-import { TestComponent } from './test/test.component';
+import { TestComponent } from './test/test.component'; // Added through the CLI
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     PersonComponent,
-    TestComponent
+    TestComponent // Added through the CLI
   ],
   imports: [
     BrowserModule
--- src/app/feature/person/person.component.ts
+++ src/app/feature/person/person.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
 
 @Component({
     selector: 'app-person',
@@ -6,10 +6,18 @@ import {Component} from '@angular/core'
     styleUrls: ['./person.component.css'],
 })
 
-export class PersonComponent {
+// For OnInit, to be recognized, be sure to import it along with
+// "Component" from @angular/core. In addtion, to implement, create
+// a method called ngOnInit() which is a lifecycle method that is 
+// ran before the component mounts but after the constructor is ran.\
+export class PersonComponent implements OnInit {
 
     constructor(){
         console.log("PersonComponent Rendering");
+    }
+
+    ngOnInit() {
+        console.log("PersonComponent ngOnInit method running")
     }
 
 }
